becba977bf671548d64a51748ecc8de713e969bb
alke <alke0001@stud.hs-kl.de>
2026-04-28 22:53:49 +0200

    feat: added new link to initial page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>Welcome to scaffy</h1>
+<p>
+	Visit <a href="https://github.com/alke0001/scaffy">scaffy GitHub</a> to read more about this project
+</p>
